0fd63345f77a37d3347496871377b44d75155950
SolaRita <ritasolacanals@gmail.com>
2022-02-03 14:18:02 +0100

    header and footer init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './componentes/header/Header';
+import Footer from './componentes/footer/Footer';
 
 function App() {
   return (
     <div>
+      <Header/>
      <h1>GREEN GO</h1>
+     <Footer/>
     </div>
   );
 }
---
+++ src/componentes/header/header.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <nav className={styles.navbar}  >
+      <div className=  {styles.img_navbar}>
+        <h1 className={styles.logo}>Green&Go</h1>
+      </div>
+    </nav>
+  );
+}
