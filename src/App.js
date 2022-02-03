1c7c93c604dc48833fdb9006e697f06610576407
SolaRita <ritasolacanals@gmail.com>
2022-02-03 21:48:22 +0100

    buttons carrito and login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './componentes/header/Header';
 import Footer from './componentes/footer/Footer';
 
+
 function App() {
   return (
     <div>
--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,11 +1,13 @@
 import styles from "./Header.module.css";
+import Login from "../buttons/Login";
+import Carrito from "../buttons/Carrito";
 
 export default function Header() {
   return (
     <nav className={styles.navbar}  >
-      <div className=  {styles.img_navbar}>
+        <Login/>
         <h1 className={styles.logo}>Green&Go</h1>
-      </div>
+        <Carrito/>
     </nav>
   );
 }
