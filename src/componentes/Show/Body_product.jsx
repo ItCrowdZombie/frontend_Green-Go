8ef480ad429d3f919bb1dec69191d88be69cae97
marteta91 <martaguitart938@gmail.com>
2022-02-06 20:18:33 +0100

    viewshow

--- src/componentes/Show/Body_product.jsx
+++ src/componentes/Show/Body_product.jsx
@@ -15,23 +15,36 @@ import "../Show/body_product.css";
             return ( 
       
             <main>
-                <div>
+                <div className="bigcontainer">
                     <div  className="ContainerProduct">
-                        <img src={require("../media/cucharitaflor.png")}  alt=""/>
-                        <div className="text">
+                        <div className="imgplant">
+                            <img src={require("../media/cucharitaflor.png")}  alt=""/>
+                        </div>
+                        {/* <img src={require("../media/Rectangle12.png")} alt=""/> */}
+                        <div className="textproduct">
                             <p>cucharita</p>
+                        </div>
+                        <div className="textshop">
+                            <p>PenguinShop</p>
+                        </div>
+                        <div className="price">
+                            <p>15,20€</p>
+                        </div>
+                        <div className="description">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div>
 
                         
                     </div>
 
-                    <div className="containerproduct">
-                        <div className="card">
-                            cards meli
+                     <div className="Allproductsshop">
+                        <div className="textcards">
+                            <p>More PenguinShop products</p>
                         </div>
-
+                        {/* <div className="cards">
+                            Aqui cards componente
+                        </div> */}
                     </div>
-
                 </div>
             </main>
 
