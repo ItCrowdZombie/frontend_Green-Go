cf6a178a68e6ea97d969aaa0491982172f176880
Carruor <fotoes10@gmail.com>
2022-02-08 10:40:02 +0100

    card style and media folder moved to public

--- src/componentes/cards/Card.jsx
+++ src/componentes/cards/Card.jsx
@@ -1,11 +1,16 @@
+import style from './Card.module.css'
+
+
 export function Card(props) {
   return (
-    <div>
-      {/* img: <img src=""></img> */}
-      name: {props.product.name}
-      description:{props.product.description}
-      price:{props.product.price}
-      seller:{props.product.seller.name}
+    <div className={style.cardStyle}>
+        <div className= {style.imgStyle}>
+        <img src={props.product.img}/>
+        </div>
+        <div className= {style.nameStyle}>name: {props.product.name}</div>
+        <div className={style.priceStyle}>price:{props.product.price}</div>
+        <div className={style.contentStyle}>description:{props.product.description}</div>
+        <div className={style.sellerStyle}>seller:{props.product.seller.name}</div>
     </div>
   );
 }
--- src/service.js
+++ src/service.js
@@ -1,23 +1,27 @@
 const cards = [
   {
+    img: "http://localhost:3000/media/planta3.jpg",
     name: "cucharita",
     seller: { name: "PenguinShop", id: 1 },
     price: 15,
     description: "decorative",
   },
   {
+    img: "http://localhost:3000/media/planta2.png",
     name: "tenedor",
     seller: { name: "PepeShop", id: 2 },
     price: 16,
     description: "decorative tambien",
   },
   {
+    img: "http://localhost:3000/media/planta1.jpg",
     name: "cuchillo thc",
     seller: { name: "PenguinShop", id: 1 },
     price: 15,
     description: "decorative",
   },
   {
+    img: "http://localhost:3000/media/planta1.jpg",
     name: "plato cbd",
     seller: { name: "PepeShop", id: 2 },
     price: 16,
