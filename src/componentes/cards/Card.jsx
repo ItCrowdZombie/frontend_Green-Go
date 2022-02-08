3c01eaab29ee909a34199aa68648992efc82e2b5
MayuRuru <aruizrull@protonmail.com>
2022-02-08 09:35:23 +0100

    merge show

---
+++ src/componentes/cards/Card.jsx
@@ -0,0 +1,11 @@
+export function Card(props) {
+  return (
+    <div>
+      {/* img: <img src=""></img> */}
+      name: {props.product.name}
+      description:{props.product.description}
+      price:{props.product.price}
+      seller:{props.product.seller.name}
+    </div>
+  );
+}
