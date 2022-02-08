cc9bb22bd36aace6fc116f0fdc979adc9b1df2ec
MayuRuru <aruizrull@protonmail.com>
2022-02-08 09:51:04 +0100

    products fake

--- src/service.js
+++ src/service.js
@@ -1,22 +1,29 @@
-
-
 const cards = [
-     {
-      name: "cucharita",
-      seller:{name:"PenguinShop", id:1},
-      price: 15,
-      description: "decorative" ,
-
-    },
-    {
-      name: "tenedor",
-      seller:{name:"PepeShop", id:2},
-      price: 16,
-      description: "decorative tambien" ,
-
-    },
-     
-]
+  {
+    name: "cucharita",
+    seller: { name: "PenguinShop", id: 1 },
+    price: 15,
+    description: "decorative",
+  },
+  {
+    name: "tenedor",
+    seller: { name: "PepeShop", id: 2 },
+    price: 16,
+    description: "decorative tambien",
+  },
+  {
+    name: "cuchillo thc",
+    seller: { name: "PenguinShop", id: 1 },
+    price: 15,
+    description: "decorative",
+  },
+  {
+    name: "plato cbd",
+    seller: { name: "PepeShop", id: 2 },
+    price: 16,
+    description: "decorative tambien",
+  },
+];
 
 export async function getAllCards() {
   return cards;
